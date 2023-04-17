8da5b7564ccf40f5a52ed93f854b894ccd45bdb8
Blobbey2000 <116673732+Blobbey2000@users.noreply.github.com>
2023-04-17 11:03:52 -0700

    Added am/pm back in

--- clock.ts
+++ clock.ts
@@ -3,27 +3,31 @@ let minutesOfTheClock = 0
 let secondsOfTheClock = 0
 let speedOfTheClock = 1000
 let clockOnOrOff = false
+let amPmTime = ""
+let amPmChange = true
 
 //% color="#C009AF"
 //% icon="\uf017"
 //% blockGap="8"
-//% groups='["Clock", "Clock Settings"]'
+//% groups="['Clock", "Clock Settings']"
 namespace Clock {
-
+    
     /**
      * Creates a customizable clock.
      */
-    //% block="set clock time to $hour : $minute : $second"
+    //% block="set clock time to $hour : $minute : $second $ampm"
     //% group="Clock"
     //% weight=2
+    //% ampm.defl="am"
     //% hour.min=1 hour.max=12
     //% minute.min=0 minute.max=59
     //% second.min=0 second.max=59
-    export function makeClock(hour: number, minute: number, second: number) {
+    export function makeClock(hour: number, minute: number, second: number, ampm: string) {
     hoursOfTheClock = hour
     minutesOfTheClock = minute
     secondsOfTheClock = second
     clockOnOrOff = true
+    amPmTime = ampm
     
     }
     
@@ -38,19 +42,19 @@ namespace Clock {
     export function clockTime(visible?: boolean) {
         if (visible) {
             if (secondsOfTheClock < 10 && minutesOfTheClock < 10) {
-                return hoursOfTheClock + ":" + "0" + minutesOfTheClock + ":" + "0" + secondsOfTheClock;
+                return hoursOfTheClock + ":" + "0" + minutesOfTheClock + ":" + "0" + secondsOfTheClock + amPmTime;
             } else if (secondsOfTheClock < 10) {
-                return hoursOfTheClock + ":" + minutesOfTheClock + ":" + "0" + secondsOfTheClock;
+                return hoursOfTheClock + ":" + minutesOfTheClock + ":" + "0" + secondsOfTheClock + amPmTime;
             } else if (minutesOfTheClock < 10) {
-                return hoursOfTheClock + ":" + "0" + minutesOfTheClock + ":" + secondsOfTheClock;
+                return hoursOfTheClock + ":" + "0" + minutesOfTheClock + ":" + secondsOfTheClock + amPmTime;
             } else {
-                return hoursOfTheClock + ":" + minutesOfTheClock + ":" + secondsOfTheClock;
+                return hoursOfTheClock + ":" + minutesOfTheClock + ":" + secondsOfTheClock + amPmTime;
             }
         } else {
             if (minutesOfTheClock < 10) {
-                return hoursOfTheClock + ":" + "0" + minutesOfTheClock;
+                return hoursOfTheClock + ":" + "0" + minutesOfTheClock + amPmTime;
             } else {
-                return hoursOfTheClock + ":" + minutesOfTheClock;
+                return hoursOfTheClock + ":" + minutesOfTheClock + amPmTime;
             }
         }
     }
@@ -107,6 +111,17 @@ forever(function() {
     }
     pause(speedOfTheClock)
     if (clockOnOrOff) {
+        amPmChange = true
+        if ((Clock.clockTime(true)) == "11:59:59am" || (Clock.clockTime(true)) == "11:59:59pm") {
+            if (amPmChange) {
+                amPmChange = false
+                if (amPmTime == "am") {
+                    amPmTime = "pm"
+                } else if (amPmTime == "pm") {
+                    amPmTime = "am"
+                }
+            }
+        }
         secondsOfTheClock += 1
         if (secondsOfTheClock >= 60) {
             secondsOfTheClock = 0
@@ -119,6 +134,5 @@ forever(function() {
         if (hoursOfTheClock >= 13) {
             hoursOfTheClock = 1
         }
-
     }
 })
